944f9b1382df7bddc33e0603ec8d4a25dd834381
Trevor Scheuermann <trevorsch1@gmail.com>
2015-11-30 13:12:28 -0600

    test deploy using heroku git

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Moseys - Under Construction' });
+  res.render('index', { title: 'Moseys - Under Construction testtsetset' });
 });
 
 module.exports = router;
