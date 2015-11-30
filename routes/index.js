441c9f8638f585f36d6ea0eb56e824aab1e11134
Trevor Scheuermann <trevorsch1@gmail.com>
2015-11-30 11:07:29 -0600

    typo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Moseys - Under Contruction' });
+  res.render('index', { title: 'Moseys - Under Construction' });
 });
 
 module.exports = router;
