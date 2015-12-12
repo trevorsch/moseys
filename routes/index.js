83bba35aa66d1c76e2ec1993888e6c15dc2e72ae
Trevor Scheuermann <trevorsch1@gmail.com>
2015-12-12 17:20:36 -0500

    menu shows. map shows

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Moseys - Under Construction' });
+  res.render('index', { title: 'Welcome to Moseys - Website Under Construction' });
 });
 
 module.exports = router;
